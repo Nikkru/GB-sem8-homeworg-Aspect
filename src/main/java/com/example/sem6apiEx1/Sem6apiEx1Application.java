e3f003ba5feefa696a3a69f84bd4a937fd441004
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-02-11 16:57:49 +0300

    delete gateway service

--- src/main/java/com/example/sem6apiEx1/Sem6apiEx1Application.java
+++ src/main/java/com/example/sem6apiEx1/Sem6apiEx1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Sem6apiEx1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Sem6apiEx1Application.class, args);
 	}
 
